d4e4f557f295cb7d9f4ee0d00297f167ad8e9d45
akwasiasamoah <akwasiemmanuel64@gmail.com>
2025-08-10 15:20:01 +0000

    find the middle of the node

--- src/SinglyLinkedList/SinglyLinkedList.java
+++ src/SinglyLinkedList/SinglyLinkedList.java
@@ -152,6 +152,10 @@ public class SinglyLinkedList {
     }
 
     public ListNode middleNode() {
+        if(head == null) {
+            return null;
+        }
+
         ListNode slowPtr = head;
         ListNode fastPtr = head;
         while(fastPtr != null && fastPtr.next != null) {
