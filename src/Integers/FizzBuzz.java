48aca39f7aa8cd12f454b5df977026fac0672910
akwasiasamoah <akwasiemmanuel64@gmail.com>
2025-08-12 18:01:25 +0000

    fizz buzz logic

---
+++ src/Integers/FizzBuzz.java
@@ -0,0 +1,29 @@
+package Integers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if(i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            }else {
+                list.add(Integer.toString(i));
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        var fizzString = fizzBuzz(100);
+        System.out.println(fizzString);
+    }
+}
